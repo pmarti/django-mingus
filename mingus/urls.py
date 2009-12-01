09a2893fe86581528654e1b61b90177e2e7c1178
Pablo Marti <pmarti@warp.es>
2009-12-01 23:46:20 +0100

    avoid 404 and 500 troubles with star import

--- mingus/urls.py
+++ mingus/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, include, patterns
+from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
